90be24173644720de80d6d9a18e6a91510098930
sningi <1620524565@qq.com>
2021-04-15 18:19:49 +0800

    up

--- atiao.py
+++ atiao.py
@@ -1,30 +1,128 @@
 import xlrd
 import os
+import matplotlib
+from matplotlib import pyplot as plt 
+
+#配置字体，显示中文
+font = {'family': 'MicroSoft Yahei',
+        'weight': 'normal',
+        'size': 8}
+matplotlib.rc("font", **font)
+
 def read_excel(filename):
     if os.path.exists(filename):
         mexcel = xlrd.open_workbook(filename)
         # 获取excel 表1
         sheet0 = mexcel.sheets()[0]
-        #列表生成式提取第一列（表头分类）的内容
+        # 列表生成式提取第一列（表头分类）的内容
         col_names = [sheet0.cell_value(0, i) for i in range(sheet0.ncols)]
         # print(col_names)
         # 各列属性为：
         # ['序号', '开课学期', '课程编号', '课程名称', '成绩', '学分', '总学时', '绩点', '考核方式', '课程属性', '课程性质']
-        
+
         # 总行数
         row = sheet0.nrows
-        for i in range(row):
-            print(sheet0.cell_value(i, 0))
-
+        all_data = []
+        for i in range(1, row):
+            # 枚举excel表格的所有行,除表头第一行
+            # 定义一门成绩的字段
+            one = {}
+            for index, name in enumerate(col_names):
+                one[name] = sheet0.cell_value(i, index)
+            # 格式化一门课程成绩信息,格式如下：
+            """
+            {
+                '序号': 67.0, '开课学期': '2020-2021-1', '课程编号': 8013232.0, 
+                '课程名称': '影视鉴赏', '成绩': 89.0,
+                '学分': 2.0, '总学时': 32.0, '绩点': '', 
+                '考核方式': '考查', '课程属性': '公选', '课程性质': '通识教育选修课'
+            }
+            """
+            all_data.append(one)
+        # 返回所有课程的成绩
+        return all_data
     else:
         print("成绩单文件不存在")
 
 
+def analyze_course(data):
+    # 统计课程各属性学分比例
+    category = dict()
+    for course in data:
+        if course['课程性质'] not in category:
+            # 第一次添加此类课程
+            category[course['课程性质']] = course['学分']
+        else:
+            # 累计这个种类课程学分
+            category[course['课程性质']] += course['学分']
+    # print(category)
+    # 统计结果样例：
+    '''
+    {
+        '通识教育选修课': 17.0, '公共必修课': 30.5, 
+        '实践课': 121.0, '学科基础课': 6.0, 
+        '学科（专业）基础课': 24.0, '通识教育必修课': 14.0, 
+        '专业必修课': 16.0, '专业选修课': 6.0
+    }
+    '''
+    return category
+
+def Pie_chart(data):
+    # 绘制学分分布饼图
+    #调节图形大小，宽，高
+    plt.figure(figsize=(7,6))
+    #定义饼状图的标签，标签是列表
+    labels = [str(i)+"(%.1f)"%(data[i]) for i in data.keys()]
+    #每个标签占多大，计算百分比
+    #总学分
+    count = 0
+    for cate in data:
+        count += data[cate]
+
+    sizes = [data[cate]/count for cate in data]
+    #将某部分外裂出来， 使用括号，将第一块分割出来，数值的大小是分割出来的与其他两块的间隙
+    explode = (0.05,0,0,0.02,0,0,0,0.05)
+
+    patches,l_text,p_text = plt.pie(sizes,explode=explode,labels=labels,
+                                    labeldistance = 1.1,autopct = '%3.1f%%',shadow = False,
+                                    startangle = 90,pctdistance = 0.6)
+
+    for t in l_text:
+        t.set_size=(30)
+    for t in p_text:
+        t.set_size=(20)
+    # 设置x，y轴刻度一致，这样饼图才能是圆的
+    plt.axis('equal')
+    plt.legend()
+    plt.show()
+
+def analyze_course_avg(data):
+    # 分析每学期的加权平均分
+    category = dict()
+    for course in data:
+        if course['开课学期'] not in category:
+            # 第一次添加这个学期成绩和学分
+            score = course['成绩']
+            weight = course['学分']
+            category[course['开课学期']] = [(score, weight)]
+        else:
+            # 累计这个学期成绩和学分
+            score = course['成绩']
+            weight = course['学分']
+            category[course['开课学期']].append((score, weight))
+    return category
 
 def main():
     data = read_excel("./成绩表.xlsx")
+    # result = analyze_course(data)
+    # Pie_chart(result)
+    avg_data = analyze_course_avg(data)
+    print(avg_data)
+
+
 
 if __name__ == "__main__":
-    #依赖安装 
+    # 依赖安装
     # pip install xlrd==1.2.0
+    # pip install matplotlib
     main()
